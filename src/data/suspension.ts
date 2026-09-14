f435d01746fb8f03a4acdc9c148cfa5bd55f596f
Udayveer525 <uvsu525@gmail.com>
2026-09-14 07:45:17 +0530

    Site up

--- src/data/suspension.ts
+++ src/data/suspension.ts
@@ -28,7 +28,7 @@
  */
 
 /** The committed default. `npm run site:down` / `site:up` rewrites this line. */
-const SUSPENDED_BY_DEFAULT = true;
+const SUSPENDED_BY_DEFAULT = false;
 
 /**
  * Netlify's env var, read at build time (this file is evaluated in Node during
